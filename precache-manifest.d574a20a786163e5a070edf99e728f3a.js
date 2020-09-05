64d9de8b24cfc7f6f801a321fc1ae2242e1eb59e
artem <spiridovichartem99@gmail.com>
2020-09-05 18:13:41 +0300

    Updates

---
+++ precache-manifest.d574a20a786163e5a070edf99e728f3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85b2c285985ed99bb8a8081074b3d1df",
+    "url": "/HB/index.html"
+  },
+  {
+    "revision": "b7719ffff342297ff317",
+    "url": "/HB/static/css/main.847c3ca4.chunk.css"
+  },
+  {
+    "revision": "fcd607f9e92e4bc0084b",
+    "url": "/HB/static/js/2.7b114452.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HB/static/js/2.7b114452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7719ffff342297ff317",
+    "url": "/HB/static/js/main.2652ed96.chunk.js"
+  },
+  {
+    "revision": "42205f68f888270ac2d9",
+    "url": "/HB/static/js/runtime-main.7fa9d789.js"
+  },
+  {
+    "revision": "1e03a0d5098cabb16c97aa2073f73929",
+    "url": "/HB/static/media/zvuk-saljuta.1e03a0d5.mp3"
+  }
+]);
